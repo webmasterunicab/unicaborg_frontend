2657157e6ea14118cea6ed97e5f09ddb583080f3
Gregory <webmasterunicab@unicab.org>
2024-08-16 22:56:47 -0500

    creación estructura básica

--- index.php
+++ index.php
@@ -3,15 +3,10 @@
 	<head>
 		<meta charset="iso-utf-8"/>
 		<link rel="stylesheet" href=""/>
-		<script type="text/javascript" src="jquery-3.0.0.min.js"></script>		
+		<script type="text/javascript" src="js/jquery-3.0.0.min.js"></script>		
 	</head>
 	<body>
-		<label>Análisis enunciados edades</label>
-		<button id="btn" onclick="">Ejecutar</button><br>
 		
-		<fieldset>
-			<legend style="font-weight: bold;"></legend>			
-		</fieldset><br>
 	  
 	</body>
 </html>
