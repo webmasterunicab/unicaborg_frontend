90587d35a1c5a6b26e7c077396050f8561d9c761
Gregory <webmasterunicab@unicab.org>
2024-08-16 22:48:45 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="iso-utf-8"/>
+		<link rel="stylesheet" href=""/>
+		<script type="text/javascript" src="jquery-3.0.0.min.js"></script>		
+	</head>
+	<body>
+		<label>Análisis enunciados edades</label>
+		<button id="btn" onclick="">Ejecutar</button><br>
+		
+		<fieldset>
+			<legend style="font-weight: bold;"></legend>			
+		</fieldset><br>
+	  
+	</body>
+</html>
